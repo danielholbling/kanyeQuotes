ee5ca479c35d14085f118655595e45db63f88dbe
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2019-10-16 16:45:57 +0200

    Functionality done

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,13 @@
 async function getQuote(){
     let data = await fetch("https://api.kanye.rest/?format=json");
     let json = data.json();
-    console.log(json);
     let quote = json.quote;
-    console.log(quote);
-    return quote;
+    return json;
 }
 
 
 async function getImageList(){
     let data = await fetch("https://api.tenor.com/v1/search?key=7HBRA4N2MEHV&q=kanye+west&limit=50");
-    // Tenor API key = 7HBRA4N2MEHV
     let json = data.json();
     return json;
 }
@@ -22,9 +19,10 @@ async function loadNewImageAndQuote(){
     imageArray = imageArray.results;
     randomIndex = Math.floor(Math.random()*50);
     img.src = imageArray[randomIndex].media[0].gif.url;
-    quote.innerText = await getQuote();
+    let quoteData = await getQuote();
+    quote.innerText = quoteData.quote;
 }
 let gurgel;
-(async () => { 
+(async () => {
     await loadNewImageAndQuote()
 })()
